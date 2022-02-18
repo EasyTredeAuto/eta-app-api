48229fcbc98b91c559c3c387a52b7c1c52d20bce
My nikname "ton <68073926+ThechoiceMaster@users.noreply.github.com>
2022-02-18 20:10:32 +0700

    Update bot-binance-trade.service.ts

--- src/bot-user/bot-binance-trade.service.ts
+++ src/bot-user/bot-binance-trade.service.ts
@@ -18,9 +18,9 @@ export class BotBinanceTradeService {
         const symbol = `${asset}${currency}`
         const balance = await this.binanceService.freeBalance(email)
         const market = await this.binanceService.getListCoinPrice(symbol)
-        if (amountType === 'amount') isAmount = amount / parseFloat(market.price)
-        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / parseFloat(market.price)
         const price = parseFloat(market.price)
+        if (amountType === 'amount') isAmount = amount / price
+        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / price
         if (balance.total[currency] < isAmount * price) throw new BadRequestException("can't buy limit in whit balance")
         const order = await this.binanceService.createLimitBuyOrder(email, symbol, isAmount, price)
         const transaction = { symbol, amount, quantity: order.amount, price: order.price, defaultBotId: body.botId, botId:body.botId, side: 'buy' } as Transaction
@@ -35,8 +35,9 @@ export class BotBinanceTradeService {
         let isAmount
         const balance = await this.binanceService.freeBalance(email)
         const market = await this.binanceService.getListCoinPrice(symbol)
-        if (amountType === 'amount') isAmount = amount / parseFloat(market.price)
-        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / parseFloat(market.price)
+        const price = parseFloat(market.price)
+        if (amountType === 'amount') isAmount = amount / price
+        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / price
         if ((balance.total[asset]  || 0) < isAmount) throw new BadRequestException("can't sell limit in whit balance")
         const order = await this.binanceService.createLimitSellOrder(email, symbol, isAmount, price)
         const transaction = { symbol, amount, quantity: order.amount, price: order.price, defaultBotId: body.botId, botId:body.botId, side: 'sell' } as Transaction
@@ -51,8 +52,9 @@ export class BotBinanceTradeService {
         let isAmount
         const balance = await this.binanceService.freeBalance(email)
         const market = await this.binanceService.getListCoinPrice(symbol)
-        if (amountType === 'amount') isAmount = amount / parseFloat(market.price)
-        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / parseFloat(market.price)
+        const price = parseFloat(market.price)
+        if (amountType === 'amount') isAmount = amount / price
+        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / price
         if ((balance.total[asset]  || 0) < isAmount) throw new BadRequestException("can't buy market in whit balance")
         const order = await this.binanceService.createMarketOrder(email, symbol, 'BUY', isAmount)
         const transaction = { symbol, amount, quantity: order.amount, price: order.price, defaultBotId: body.botId, botId:body.botId, side: 'buy' } as Transaction
@@ -67,8 +69,9 @@ export class BotBinanceTradeService {
         let isAmount
         const balance = await this.binanceService.freeBalance(email)
         const market = await this.binanceService.getListCoinPrice(symbol)
-        if (amountType === 'amount') isAmount = amount / parseFloat(market.price)
-        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / parseFloat(market.price)
+        const price = parseFloat(market.price)
+        if (amountType === 'amount') isAmount = amount / price
+        else isAmount = (parseFloat(balance.total[currency]) * (amount / 100)) / price
         if ((balance.total[asset]  || 0) < isAmount) throw new BadRequestException("can't sell market in whit balance")
         const order = await this.binanceService.createMarketOrder(email, symbol, 'SELL', isAmount)
         const transaction = { symbol, amount, quantity: order.amount, price: order.price, defaultBotId: body.botId, botId:body.botId, side: 'sell' } as Transaction
